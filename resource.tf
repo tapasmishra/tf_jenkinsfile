642005d923ed92ba4fc7cad41bab7b8fd9c0a270
mohinali <94956303+mohinali@users.noreply.github.com>
2022-06-16 19:16:49 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,38 @@
+resource "openstack_networking_floatingip_v2" "myip" {
+  pool = "${var.public_network_name}"
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/templates/conf_data.yaml")
+}
+
+resource "openstack_compute_keypair_v2" "demo_keypair" {
+  name       = "${var.openstack_key}"
+  public_key = var.public_key
+}
+
+resource "openstack_networking_port_v2" "ports" {
+  count          = 1 
+  name           = "${format("port-%02d", count.index + 1)}"
+  network_id     = "${var.private_network_id}"
+  admin_state_up = "true"
+  port_security_enabled = "false"
+}
+
+resource "openstack_compute_instance_v2" "agw_deployment"{
+  name            = "${var.prefix}"
+  image_id        = "${var.image_id}"
+  flavor_id       = "${var.flavor_id}"
+  key_pair        = openstack_compute_keypair_v2.demo_keypair.name
+  user_data       = data.template_file.user_data.rendered
+network {
+    port = "${openstack_networking_port_v2.ports.*.id[0]}"
+  }
+}
+
+
+resource "openstack_networking_floatingip_associate_v2" "myip" {
+  floating_ip = "${openstack_networking_floatingip_v2.myip.address}"
+  port_id = "${openstack_networking_port_v2.ports.*.id[0]}"
+}
+

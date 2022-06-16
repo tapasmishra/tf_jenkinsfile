332392943e793863051bbca4cb553166d1d19865
mohinali <94956303+mohinali@users.noreply.github.com>
2022-06-16 19:15:22 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+    required_version = ">= 1.1.9"
+    required_providers {
+         openstack = {
+         source = "terraform-provider-openstack/openstack"
+         version = "~> 1.35.0"
+         }
+       }
+     }
+
+provider "openstack" {
+      user_name = var.openstack_username
+      password = var.openstack_password
+      auth_url = var.openstack_auth_url
+      domain_name = var.openstack_domain
+      tenant_name = var.openstack_project
+     }
